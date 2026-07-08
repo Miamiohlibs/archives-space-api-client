f422a4da5f35902badfe71a934527582d6e8bff9
Ken Irwin <ken@kenirwin.net>
2026-07-08 15:06:03 -0400

    added archival_objects test

--- test/getUrl.test.ts
+++ test/getUrl.test.ts
@@ -5,6 +5,8 @@ import AspaceClient from '../dist/AspaceClient.js';
 import { z } from 'zod';
 import { repoResourcesSchema } from '../dist/schemas/repoResourcesSchema';
 import { repoTopContainerSchema } from '../dist/schemas/RepoTopContainersSchema.js';
+import { repoArchivalObjectSchema } from '../dist/schemas/RepoArchivalObjectsSchema.js';
+
 const baseUrl = process.env.ASPACE_BASE_URL || '';
 const username = process.env.USERNAME || '';
 const password = process.env.PASSWORD || '';
@@ -36,6 +38,20 @@ describe('getUrl', () => {
       'https://archivesstaff.lib.miamioh.edu/api/repositories/2/top_containers/7836';
     const raw = await client.getUrl(url);
     const results = repoTopContainerSchema.parse(raw);
+    expect(results).toEqual(raw);
+  });
+
+  it('should get a archival_objects URL and it should parse ok', async () => {
+    const client = new AspaceClient({
+      baseUrl,
+      username,
+      password,
+    });
+    await client.getToken();
+    const url =
+      'https://archivesstaff.lib.miamioh.edu/api/repositories/2/archival_objects/5616';
+    const raw = await client.getUrl(url);
+    const results = repoArchivalObjectSchema.parse(raw);
     expect(results).toEqual(raw);
   });
 });
