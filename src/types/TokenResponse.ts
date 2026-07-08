4a2504379d1d14bcdb4be8d97bb230ca9b9d4f74
Ken Irwin <ken@kenirwin.net>
2026-07-08 09:11:10 -0400

    remove types folder in favor of infered zod types

--- src/types/TokenResponse.ts
+++
@@ -1,8 +0,0 @@
-export type TokenResponse =
-  | { success: true; token: string; user: any; error?: never }
-  | {
-      success: false;
-      token?: never;
-      user?: never;
-      error: { status: number; message: string };
-    };
